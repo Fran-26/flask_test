b65afa05cf265f124c90852a5eae7bbc27d89661
Fran-26 <fservin@outlook.com>
2020-06-10 00:31:38 -0400

    first try

---
+++ application.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	name= "Hello, world2!"
+	return render_template("index.html", name = name)
+
+@app.route("/<string:name>")
+def hello(name):
+	return "Hello, %s !" % name
+
