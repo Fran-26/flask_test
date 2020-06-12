b249e9bf7a24f2dec1c92fbd03722d7ce74b9aef
Fran-26 <30328842+Fran-26@users.noreply.github.com>
2020-06-12 01:19:17 -0400

    correccion de AIO_key

--- static/sensores.js
+++ static/sensores.js
@@ -6,7 +6,7 @@ var armado = 0;
 var puerta_1, ventana_1, ventana_2, temperatura_1;
 
 function update (){
-	var reqURL = 'https://io.adafruit.com/api/v2/veintiseis/groups/default?x-aio-key=aio_Nqnb30XqNIhapRSfk2XXijFhkvpd'
+	var reqURL = 'https://io.adafruit.com/api/v2/veintiseis/groups/default?x-aio-key=aio_RRpJ09FUiTclcBHh2XCqB2xMBGyW'
 	var req = new XMLHttpRequest();
 	req.open('GET', reqURL, true);
 	req.onreadystatechange = function () {
