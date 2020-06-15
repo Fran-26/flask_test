d1df1caca53c1be98f32efcd10d585eeeb96cbe9
Fran-26 <30328842+Fran-26@users.noreply.github.com>
2020-06-15 18:33:40 -0400

    Update list.py

--- list.py
+++ list.py
@@ -7,7 +7,7 @@ engine = create_engine("postgresql://pi:0000@localhost:5432/home")
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
-    REGISTRO = db.execute("SELECT * FROM REGISTRO").fetchall()
+    REGISTRO = db.execute("SELECT * FROM REGISTRO JOIN SENSORES ON SENSORES.ID=REGISTRO.ID").fetchall()
     for registro in REGISTRO:
         print(registro.sensor_id, " set to ", registro.estado, " at ", registro.tiempo)
 
