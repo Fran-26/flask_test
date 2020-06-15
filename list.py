55a38f8b851cf20d1d3ce6c1c24ea18435073d4c
Fran-26 <30328842+Fran-26@users.noreply.github.com>
2020-06-15 01:22:17 -0400

    Update list.py

--- list.py
+++ list.py
@@ -3,14 +3,14 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(os.getenv('postgresql://pi:pi@localhost:5432/home'))
+engine = create_engine("postgresql://pi:0000@localhost:5432/home")
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
-    flights = db.execute("SELECT * FROM REGISTRO").fetchall()
+    REGISTRO = db.execute("SELECT * FROM REGISTRO").fetchall()
     for registro in REGISTRO:
         print(":id set to :estado at :time",
-        {"id":registro.SENSOR_ID,"estado":registro.ESTADO,"tiempo":registro.TIEMPO})
+        {"id":registro.sensor_id,"estado":registro.estado,"tiempo":registro.tiempo})
 
 if __name__ == "__main__":
     main()
