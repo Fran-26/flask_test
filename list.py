666b41e8703e034c8df171e52edf96f12f523d85
Fran-26 <30328842+Fran-26@users.noreply.github.com>
2020-06-15 19:00:37 -0400

    Update list.py

--- list.py
+++ list.py
@@ -9,7 +9,7 @@ db = scoped_session(sessionmaker(bind=engine))
 def main():
     REGISTRO = db.execute("SELECT * FROM REGISTRO JOIN SENSORES ON SENSORES.ID=REGISTRO.SENSOR_ID").fetchall()
     for registro in REGISTRO:
-        print(registro.sensor_id, " set to ", registro.estado, " at ", registro.tiempo)
+        print(registro.sensor, " set to ", registro.estado, " at ", registro.tiempo)
 
 if __name__ == "__main__":
     main()
