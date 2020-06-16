b04c372c2591691a3a14aedadb3ea7089e28f44b
Fran-26 <fservin@outlook.com>
2020-06-16 15:56:01 -0400

    asdsa

--- list.py
+++ list.py
@@ -9,15 +9,7 @@ db = scoped_session(sessionmaker(bind=engine))
 def main():
     REGISTRO = db.execute("SELECT * FROM ESTADO JOIN SENSORES ON SENSORES.ID=ESTADO.ID").fetchall()
     for registro in REGISTRO:
-<<<<<<< HEAD
-<<<<<<< HEAD
-        print(registro.sensor_id, " set to ", registro.estado, " at ", registro.tiempo)
-=======
-        print(registro.sensor, " set to ", registro.estado, " at ", registro.tiempo)
-=======
         print(registro.sensor, " set to ", registro.estado)
->>>>>>> cd922ffaa882e411bd2c838dcc46d0329fb12f31
 
->>>>>>> 666b41e8703e034c8df171e52edf96f12f523d85
 if __name__ == "__main__":
     main()
