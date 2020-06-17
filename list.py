d48006a6f0b3d8192c150865d80e3da2b27a4de0
Fran-26 <30328842+Fran-26@users.noreply.github.com>
2020-06-17 00:02:04 -0400

    Update list.py

--- list.py
+++ list.py
@@ -10,6 +10,16 @@ def main():
     REGISTRO = db.execute("SELECT * FROM ESTADO JOIN SENSORES ON SENSORES.ID=ESTADO.ID").fetchall()
     for registro in REGISTRO:
         print(registro.sensor, " set to ", registro.estado)
-
+	id = "3"
+	estado = "True"
+	x="INSERT INTO REGISTRO (ID, ESTADO) VALUES ({id}, {estado});".format(id=id, estado=estado)
+	print(x)
+	db.execute(x)
+	db.commit
+	y="UPDATE ESTADO SET ESTADO={estado} WHERE ID={id};".format(id=id, estado=estado)
+	print(y)
+	db.execute(y)
+	db.commit
+		
 if __name__ == "__main__":
     main()
