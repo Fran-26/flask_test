40108c9552dd16e08b55249f1761e8d04ff780fc
Fran-26 <30328842+Fran-26@users.noreply.github.com>
2020-06-16 23:10:06 -0400

    Update application.py

--- application.py
+++ application.py
@@ -65,10 +65,10 @@ def set(id, estado1):
 	try:
 		db.execute("INSERT INTO REGISTRO (ID, ESTADO) VALUES ({id}, {estado})".format(id=id, estado=estado))
 		db.commit
-"""		x="UPDATE ESTADO SET ESTADO={estado} WHERE ID={id}".format(id=id, estado=estado)
-		print(x)
-		db.execute(x)
-		db.commit"""
+		#x="UPDATE ESTADO SET ESTADO={estado} WHERE ID={id}".format(id=id, estado=estado)
+		#print(x)
+		#db.execute(x)
+		#db.commit
 		print ("funciono2\n\n\n\n\n\n\n\n\n\n\n")
 	except:
 		return "Algo salio mal"
