524dfce6e1c5ea500097a4d417d9f981eef2c62d
Fran-26 <30328842+Fran-26@users.noreply.github.com>
2020-06-17 00:29:22 -0400

    Update application.py

--- application.py
+++ application.py
@@ -37,6 +37,7 @@ def luces():
 @app.route("/valores")
 def valores():
 	valores = db.execute("SELECT * FROM ESTADO JOIN SENSORES ON SENSORES.ID=ESTADO.ID").fetchall()
+	dictionary={}
 	for registro in valores:
 		if registro.estado:
 			dictionary [registro.sensor] = "cerrado"
