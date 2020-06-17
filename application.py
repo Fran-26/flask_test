857479a9f2ed34cbcc7e8ec5ac6cdfb28a5c7b3d
Fran-26 <30328842+Fran-26@users.noreply.github.com>
2020-06-16 23:12:03 -0400

    Update application.py

--- application.py
+++ application.py
@@ -51,6 +51,7 @@ def valores():
 	
 @app.route("/set/<int:id>/<string:estado1>")
 def set(id, estado1):
+	print (id," : ", estado1, "\n\n\n\n\n\n\n\n\n\n\n")
 	query=db.execute("SELECT * FROM SENSORES WHERE ID= {}".format(id)).fetchone()
 	print (query)
 	
@@ -63,12 +64,15 @@ def set(id, estado1):
 		estado = False
 		
 	try:
-		db.execute("INSERT INTO REGISTRO (ID, ESTADO) VALUES ({id}, {estado})".format(id=id, estado=estado))
+		x="INSERT INTO REGISTRO (ID, ESTADO) VALUES ({id}, {estado})".format(id=id, estado=estado)
+		print(x)
+		db.execute(x)
 		db.commit
-		#x="UPDATE ESTADO SET ESTADO={estado} WHERE ID={id}".format(id=id, estado=estado)
-		#print(x)
-		#db.execute(x)
-		#db.commit
+		print ("funciono1\n\n\n\n\n\n\n\n\n\n\n")
+		x="UPDATE ESTADO SET ESTADO={estado} WHERE ID={id}".format(id=id, estado=estado)
+		print(x)
+		db.execute(x)
+		db.commit
 		print ("funciono2\n\n\n\n\n\n\n\n\n\n\n")
 	except:
 		return "Algo salio mal"
