c50b3edc1864ae0294567b880b1d2619af80f360
Fran-26 <30328842+Fran-26@users.noreply.github.com>
2020-08-26 18:37:37 -0400

    Update application.py

--- application.py
+++ application.py
@@ -70,9 +70,8 @@ def set(id, estado):
 	db.commit()
 
 	for i in range(len(tablaEstado)):
-		if tablaEstado[i]["id"] == id:
+		if int(tablaEstado[i]["id"]) == id:
 			tablaEstado[i]["estado"] = estado
-			print(tablaEstado[i])
 
 	x="{id} guardado {estado}".format(id=id, estado=estado)
 	return (x)
